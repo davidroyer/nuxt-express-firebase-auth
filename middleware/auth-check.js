92d0611be855dc5a3bfb6927c29d7655e47dc2dd
David Royer <droyer01@gmail.com>
2017-10-18 03:54:48 -0400

    update api route

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,9 +1,5 @@
 export default function ({ store, redirect, error }) {
   if (!store.state.loggedIn) {
       redirect('/login')
-    // error({
-    //   message: 'You are not connected',
-    //   statusCode: 403
-    // })
   }
 }
--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,10 @@ import users from './users'
 const router = Router()
 
 // Add USERS Routes
+router.get('/', function (req, res, next) {
+  res.json({ result: 'Main Api Page' })
+})
+
 router.use(users)
 
 export default router
